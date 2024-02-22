9c80297f0dfb5b0189e63d5fc126ba34f3c6f604
Edvaldo Melquiades <melquiades.emc@gmail.com>
2024-02-22 11:29:41 -0300

    changed asset names

--- web-build/Extensions/Firebase/B_firebasetools/D_remoteconfigtools.js
+++
@@ -1,2 +0,0 @@
-var gdjs;(function(e){let r;(function(a){let p;(function(n){let s;(function(i){i.setAutoUpdateInterval=t=>{firebase.remoteConfig().settings.minimumFetchIntervalMillis=t},i.setDefaultConfig=t=>{firebase.remoteConfig().defaultConfig=t.toJSObject()},e.evtTools.firebaseTools.onAppCreated.push(()=>{firebase.remoteConfig().settings.minimumFetchIntervalMillis=-1})})(s=n.remoteConfig||(n.remoteConfig={}))})(p=a.firebaseTools||(a.firebaseTools={}))})(r=e.evtTools||(e.evtTools={}))})(gdjs||(gdjs={}));
-//# sourceMappingURL=D_remoteconfigtools.js.map
